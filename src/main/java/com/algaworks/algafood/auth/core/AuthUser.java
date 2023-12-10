447045e6e08d2c999aeb491adbe73612907ac438
Idevaldo Neto <inetomec@yahoo.com.br>
2023-12-10 18:42:31 -0300

    feat: Criada classe que recebe os dados do usuário a ser autenticado
    Aula: 23.14. Autenticando usuário com dados do banco de dados

---
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.auth.core;
+
+import com.algaworks.algafood.auth.domain.model.Usuario;
+import lombok.Getter;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collections;
+
+/**
+ * Classe que recebe os dados do usuário a ser autenticado
+ *
+ * @author Idevaldo Neto <idevbn@gmail.com>
+ */
+@Getter
+public class AuthUser extends User {
+
+    private String fullName;
+
+    public AuthUser(final Usuario usuario) {
+        super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+
+        this.fullName = usuario.getNome();
+    }
+
+}
