11865f3404a0c9b6d487316f29139cf24572aa3e
Idevaldo Neto <inetomec@yahoo.com.br>
2023-12-10 18:16:34 -0300

    feat: Adicionados model e repository de Usuario
    Aula: 23.14. Autenticando usuário com dados do banco de dados

---
+++ src/main/java/com/algaworks/algafood/auth/domain/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.auth.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "tb_usuario")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Usuario {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+}
