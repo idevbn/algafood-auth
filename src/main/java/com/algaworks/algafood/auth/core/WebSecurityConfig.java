487ef1527ec8273c1ca0a1e060ea4497c87ddddc
Idevaldo Neto <inetomec@yahoo.com.br>
2023-12-10 18:15:45 -0300

    refactor: Alterados pacotes das classes do projeto
    Aula: 23.14. Autenticando usuário com dados do banco de dados
    
    Os métodos sobrescritos configure e userDetails foram removidos

--- src/main/java/com/algaworks/algafood/auth/core/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/WebSecurityConfig.java
@@ -1,30 +1,16 @@
-package com.algaworks.algafood.auth;
+package com.algaworks.algafood.auth.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user_system")
-                    .password(this.passwordEncoder().encode("123456"))
-                    .roles("ADMIN")
-                .and()
-                .withUser("user_system2")
-                    .password(this.passwordEncoder().encode("123456"))
-                    .roles("ADMIN");
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -35,12 +21,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
-    }
-
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return super.userDetailsService();
     }
 
 }
