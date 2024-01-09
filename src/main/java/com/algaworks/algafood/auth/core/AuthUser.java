0cbdcc20679841949fa0768fe4e6f61aed892507
Idevaldo Neto <inetomec@yahoo.com.br>
2024-01-08 23:03:19 -0300

    refactor: Removida importação não utilizada
    Aula: 23.26. Restringindo acesso a endpoints por escopos do OAuth2

--- src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
-import java.util.Collections;
 
 /**
  * Classe que recebe os dados do usuário a ser autenticado
